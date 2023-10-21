a756b92edc9d78de8e970b358155e3c6fd6b8213
Omru Jawara <omaru.jawara94@gmail.com>
2023-10-21 18:47:23 -0400

    Create CPU internals

---
+++ cpu6502/CPU.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Datatypes.h"
+
+// 6502 Proccessor
+struct CPU
+{
+    word PC; // Program Counter
+    word SP; // Stack Pointer
+
+    // Three 8 bits registers respectively
+    byte A; // A -> Accumulator
+    byte X; // X -> Index Register X
+    byte Y; // Y -> Index Register Y
+
+    /*
+        Proccessor Status Flags
+        Using the bit-field feature to occupy 1 bit for each flag
+    */
+    byte C : 1; // CARRY FLAG 0=FALSE 1=TRUE
+    byte Z : 1; // ZERO FLAG 0=RESULT NOT ZERO 1=RESULT ZERO
+    byte I : 1; // IRQ DISABLE FLAG 0=ENABLE 1=DISABLE
+    byte D : 1; // DECIMAL MODE FLAG 0=FALSE 1=TRUE
+    byte B : 1; // BREAK COMMAND FLAG 0= NO BREAK 1=BREAK
+    byte V : 1; // OVERFLOW FLAG 0=FALSE 1=TRUE
+    byte N : 1; // MEGATIVE FLAGE 0=POSITIVE 1=NEGATIVE
+
+    // opcodes
+    static constexpr byte INS_LDA_IM = 0xA9; // Load Accumulator (Immediate)
+
+};
---
+++ cpu6502/Datatypes.h
@@ -0,0 +1,5 @@
+#pragma once
+
+using byte = unsigned char;  // a byte(8 bits)
+using word = unsigned short; // a word(2 bytes -> (16 bits) )
+using u32 = unsigned int;    // 4 bytes -> 32 bits;
